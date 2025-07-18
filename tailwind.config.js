5574d711bc7dac215ba7acb60dd765a86afe7152
Leandro Larson <leandrolarson@alunos.utfpr.edu.br>
2025-07-18 17:38:24 -0300

    Versão inicial do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-blue": "#022A6A",
+        "brand-yellow": "#E7B901",
+      },
+    },
+  },
+  plugins: [],
+};
