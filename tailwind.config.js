058d3cde7e68ae2d270e485032e205a38b460e31
Leandro Larson <leandrolarson@alunos.utfpr.edu.br>
2026-05-11 09:07:33 -0300

    update paths - tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.{html,js}",
-    "./js/**/*.js", // Adicionamos a pasta js para o Tailwind monitorizar
-  ],
+  content: ["./*.{html,js}", "./admin/**/*.{html,js}", "./js/**/*.js"],
   theme: {
     extend: {
       colors: {
